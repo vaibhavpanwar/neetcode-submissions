7cacd2d14dbe5b8cf6a9536dbb5a2294b34d7432
Vaibhav Panwar <51419252+vaibhavpanwar@users.noreply.github.com>
2026-05-26 12:19:15 +0530

    Add: merge-k-sorted-linked-lists - submission-1

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-1.ts
@@ -0,0 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {ListNode[]} lists
+     * @return {ListNode}
+     */
+    mergeKLists(lists: ListNode[]): ListNode {
+        if (!lists.length) return null;
+
+        function mergeTwoSorted(list1, list2) {
+            let list = new ListNode();
+            let tail = list;
+            while (list1 && list2) {
+                if (list1.val <= list2.val) {
+                    tail.next = list1;
+                    list1 = list1.next;
+                } else {
+                    tail.next = list2;
+                    list2 = list2.next;
+                }
+                tail = tail.next;
+            }
+
+            tail.next = list1 || list2;
+            return list.next;
+        }
+        function merge(array: ListNode[]) {
+            if (array.length <= 1) return array[0];
+            let mid = Math.floor(array.length / 2);
+            let left = merge(array.slice(0, mid));
+            let right = merge(array.slice(mid));
+            return mergeTwoSorted(left, right);
+        }
+
+        return merge(lists);
+    }
+}
