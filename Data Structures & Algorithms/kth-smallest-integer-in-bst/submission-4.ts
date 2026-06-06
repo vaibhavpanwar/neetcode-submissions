979dde5b94c98442d10ea82e070b2a9bd12591d3
Vaibhav Panwar <51419252+vaibhavpanwar@users.noreply.github.com>
2026-06-06 12:42:57 +0530

    Add: kth-smallest-integer-in-bst - submission-4

---
+++ Data Structures & Algorithms/kth-smallest-integer-in-bst/submission-4.ts
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @param {number} k
+     * @return {number}
+     */
+    kthSmallestWithArray(root: TreeNode | null, k: number): number {
+        let results: number[] = [];
+
+        function traverse(rootNode: TreeNode | null) {
+            if (rootNode.left) traverse(rootNode.left);
+            results.push(rootNode.val);
+            if (rootNode.right) traverse(rootNode.right);
+        }
+        traverse(root);
+        return results[k - 1] || null;
+    }
+    // TC --> O(H + K)--> O(n) worsrt
+    // SC --> O(N)
+    kthSmallest(root: TreeNode | null, k: number): number {
+        let curr=root;
+        let stack=[]
+
+        while(curr || stack.length){
+            while(curr){
+                stack.push(curr);
+                curr=curr.left;
+            }
+            curr=stack.pop();
+            k--;
+            if(k===0) return curr.val;
+            curr=curr.right
+        }
+    }
+    // TC --> O(H + K)--> O(n) worsrt
+    // SC --> O(H) stack
+
+}
