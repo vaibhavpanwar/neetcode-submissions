d9edbfa2b26c81abcc8665d09fc3b44b4677c456
Vaibhav Panwar <51419252+vaibhavpanwar@users.noreply.github.com>
2026-06-06 15:59:34 +0530

    Add: lowest-common-ancestor-in-binary-search-tree - submission-3

---
+++ Data Structures & Algorithms/lowest-common-ancestor-in-binary-search-tree/submission-3.ts
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @param {TreeNode} p
+     * @param {TreeNode} q
+     * @return {TreeNode}
+     */
+    lowestCommonAncestorRecusrive(root: TreeNode | null, p: TreeNode | null, q: TreeNode | null) {
+        if (p.val < root.val && q.val < root.val) {
+            return this.lowestCommonAncestorRecusrive(root.left, p, q);
+        }
+        if (p.val > root.val && q.val > root.val) {
+            return this.lowestCommonAncestorRecusrive(root.right, p, q);
+        }
+        return root;
+    }
+    // TC--> O(H)
+    // SC -->O(H) recursive function stack, (O(N) for skewed trees
+    lowestCommonAncestor(root: TreeNode | null, p: TreeNode | null, q: TreeNode | null) {
+        while (root) {
+            if (p.val < root.val && q.val < root.val) {
+                root = root.left;
+            } else if (p.val > root.val && q.val > root.val) {
+              
+                root = root.right;
+            } else 
+            {
+                return root;
+            }
+        }
+
+        return root;
+    }
+    //TC--> O(H) (O(N)) for skewed
+    //SC (O(1))--> no extra space is used 
+}
