ae61cc72aab636ea529df18bfe82fdf7f1b1c33f
Vaibhav Panwar <51419252+vaibhavpanwar@users.noreply.github.com>
2026-06-09 09:59:13 +0530

    Add: combination-target-sum - submission-0

---
+++ Data Structures & Algorithms/combination-target-sum/submission-0.ts
@@ -0,0 +1,34 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @returns {number[][]}
+     */
+    combinationSum(nums: number[], target: number): number[][] {
+      let result:number[][]=[];
+      
+      function combination(ind:number,array:number[], target:number, finalResult: number[][], temp:number[]){
+       // when index is reached we have to check on any pick non pick choice wether we have resached the targett
+       if(ind===array.length){
+        target===0 && finalResult.push([...temp])
+        return;
+       };
+      
+      // PICK --> we can only pickk when the desired result is less than zero
+      if(target-array[ind]>=0){
+        // add the pick to the ds
+        temp.push(array[ind]);
+        combination(ind, array, target-array[ind],finalResult, temp);
+       // remove the added element for non pick choice
+        temp.pop()
+      }
+      combination(ind+1,array, target, finalResult, temp)
+       
+
+      }
+
+      combination(0,nums, target,result,[])
+      return result;
+  
+      }
+}
