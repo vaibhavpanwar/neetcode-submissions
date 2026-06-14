4459b7ebb9b66383989f8af2fbb5cdc1f878117e
Vaibhav Panwar <51419252+vaibhavpanwar@users.noreply.github.com>
2026-06-14 22:22:31 +0530

    Add: course-schedule - submission-4

---
+++ Data Structures & Algorithms/course-schedule/submission-4.ts
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {number} numCourses
+     * @param {number[][]} prerequisites
+     * @return {boolean}
+     */
+    canFinish(numCourses: number, prerequisites: number[][]): boolean {
+        let adjacenyList: Record<number, number[]> = {};
+        let visited = new Set<number>();
+
+        for (let i = 0; i < numCourses; i++) adjacenyList[i] = [];
+
+        for (let [course, pre] of prerequisites) adjacenyList[course].push(pre);
+
+        function dfs(course: number): boolean {
+            if (visited.has(course)) return false;
+            if (!adjacenyList[course].length) return true;
+            visited.add(course);
+            for (let pre of adjacenyList[course]) {
+                if (!dfs(pre)) return false;
+            }
+            visited.delete(course);
+            return true;
+        }
+
+        for (let i = 0; i < numCourses; i++) {
+            if (!dfs(i)) return false;
+        }
+        return true;
+    }
+}
