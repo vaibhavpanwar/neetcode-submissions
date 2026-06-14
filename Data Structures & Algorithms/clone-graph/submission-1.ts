87e9e4077e98779bc8886091ab4a470b62582133
Vaibhav Panwar <51419252+vaibhavpanwar@users.noreply.github.com>
2026-06-14 09:57:38 +0530

    Add: clone-graph - submission-1

---
+++ Data Structures & Algorithms/clone-graph/submission-1.ts
@@ -0,0 +1,36 @@
+/**
+ * // Definition for a Node.
+ * class Node {
+ *     constructor(val = 0, neighbors = []) {
+ *       this.val = val;
+ *       this.neighbors = neighbors;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {Node} node
+     * @return {Node}
+     */
+    cloneGraph(node: Node | null): Node {
+        let visited={}
+        // will store clones against node.valuess
+        function dfs(node:Node|null){
+            if(!node) return null;
+            if(visited[node.val]) return visited[node.val];
+
+            let newNode=new Node(node.val);
+            visited[node.val]=newNode;
+
+            //recursively populate neighbours;
+            for(let neighbor of node.neighbors){
+                newNode.neighbors.push(dfs(neighbor))
+            }
+            return newNode
+        }
+
+        return dfs(node)
+
+    }
+}
