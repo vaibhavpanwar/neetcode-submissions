e6e3870c41c386320ff446ec1dcbd9e0f2133b01
Vaibhav Panwar <51419252+vaibhavpanwar@users.noreply.github.com>
2026-06-21 17:25:02 +0530

    Add: house-robber - submission-2

---
+++ Data Structures & Algorithms/house-robber/submission-2.ts
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    rob(nums: number[]): number {
+        let num1 = 0,
+            num2 = 0;
+        // we can store at each position what is the maximum total sum we can rob so far
+        // at 1 we can only rob 1 so thats the max
+        // at 2 we can either use 2 or we could use the previous max
+        // now at 3 we can either use 1 plus 3 or use rob 2 whicever is maximum
+        /*
+[2,9,8,3,6]
+[2,9,(8+2 or 9)10, (9+3 or 10)12, (10+6) or 12  ]
+//dp[i]=Math.max(dp[i-2]+nums[i], dp[i-1])
+
+*/
+        if (nums.length <= 2) return Math.max(nums[0], nums[1] || 0);
+        let dp = [nums[0], Math.max(nums[0], nums[1])];
+        for(let i=2;i<nums.length;i++){
+            dp[i]=Math.max(nums[i]+dp[i-2],dp[i-1])
+        }
+        return dp[nums.length-1]
+    }
+}
