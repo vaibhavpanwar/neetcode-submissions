0cd24e58a08aa6c4b00b3d29d1b7878da0bf7167
Vaibhav Panwar <51419252+vaibhavpanwar@users.noreply.github.com>
2026-06-27 11:48:34 +0530

    Add: count-paths - submission-1

---
+++ Data Structures & Algorithms/count-paths/submission-1.ts
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number} m
+     * @param {number} n
+     * @return {number}
+     */
+    uniquePaths(m: number, n: number): number {
+        let dp:number[][]=[]
+        function check(r,c):number{
+            if(r>=m||c>=n) return 0
+            if(dp[r]&& dp[r][c]!==undefined) return dp[r][c];
+            if(r===m-1&& c===n-1)return 1;
+            if(r===m-1)return 1;
+            if(c===n-1) return 1;
+            let result=check(r+1,c)+check(r,c+1);
+            if(!dp[r])dp[r]=[]
+            dp[r][c]=result;
+            return result  
+
+        }
+        return check(0,0)
+    }
+}
