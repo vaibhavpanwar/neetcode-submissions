f0b094faa01e543dd304e1af69e42280341b8822
Vaibhav Panwar <51419252+vaibhavpanwar@users.noreply.github.com>
2026-06-28 19:51:41 +0530

    Add: reverse-bits - submission-1

---
+++ Data Structures & Algorithms/reverse-bits/submission-1.ts
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {number} n - a positive integer
+     * @return {number} - a positive integer
+     */
+    reverseBits(n: number): number {
+        let res = 0;
+
+        for (let i = 0; i < 32; i++) {
+            res = (res << 1) | (n & 1);
+            n >>>= 1;
+        }
+
+        return res>>>0;
+    }
+}
