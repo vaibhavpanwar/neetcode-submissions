3cc74de511be294247fde6c2ae4de83036f15f9d
Vaibhav Panwar <51419252+vaibhavpanwar@users.noreply.github.com>
2026-07-02 14:24:52 +0530

    Add: non-overlapping-intervals - submission-9

---
+++ Data Structures & Algorithms/non-overlapping-intervals/submission-9.ts
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {number[][]} intervals
+     * @return {number}
+     */
+    eraseOverlapIntervals(intervals: number[][]): number {
+        intervals.sort((a, b) => a[0] - b[0]);
+        // we will use greedy approach
+        // ha, last end value store krenge
+        // agar ab agle element ki start value lasrt end se chhoti hai to mtlb overlap
+        // ab overlap k case me ham bdi end wali value htadenge taki aage overlap k km chances ho or min wale ko chose krleenge
+        let count = 0,
+            lastEndVal = intervals[0][1];
+        for (let i = 1; i < intervals.length; i++) {
+            let [start, end] = intervals[i];
+            if (start >= lastEndVal) {
+                //not overlapping;
+                lastEndVal = end;
+            } else {
+                count++;
+                lastEndVal = Math.min(lastEndVal, end);
+            }
+        }
+        return count;
+    }
+}
