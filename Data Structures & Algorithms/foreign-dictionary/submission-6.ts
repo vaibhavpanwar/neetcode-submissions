6aa4505ce6348ef8906914563d4f1ea53ddfc629
Vaibhav Panwar <51419252+vaibhavpanwar@users.noreply.github.com>
2026-07-24 16:56:43 +0530

    Add: foreign-dictionary - submission-6

---
+++ Data Structures & Algorithms/foreign-dictionary/submission-6.ts
@@ -0,0 +1,130 @@
+class Solution {
+    /**
+     * @param {string[]} words
+     * @returns {string}
+     */
+    foreignDictionary1(words: string[]): string {
+        let adjacencyList = {};
+        for (let word of words) {
+            for (let char of word) {
+                adjacencyList[char] = new Set();
+            }
+        }
+        for (let i = 0; i < words.length - 1; i++) {
+            let word1 = words[i];
+            let word2 = words[i + 1];
+            let minLength = Math.min(word1.length, word2.length);
+            // invalid condition like abcd, abc
+            if (
+                word1.length > word2.length &&
+                word1.slice(0, minLength) === word2.slice(0, minLength)
+            ) {
+                return "";
+            }
+
+            for (let j = 0; j < word1.length; j++) {
+                if (word1[j] !== word2[j]) {
+                    adjacencyList[word1[j]].add(word2[j]);
+                    break;
+                }
+            }
+        }
+        const visited = {};
+        let result = [];
+        function dfs(node) {
+            if (visited.hasOwnProperty(node)) return visited[node];
+            visited[node] = true;
+            for (let neighbour of adjacencyList[node]) {
+                if (dfs(neighbour)) return true;
+            }
+            visited[node] = false;
+            result.push(node);
+        }
+        for (let n in adjacencyList) {
+            if (dfs(n)) return "";
+        }
+        return result.reverse().join("");
+    }
+    foreignDictionaryT(words: string[]): string {
+        let aL = {};
+        for (let word of words) {
+            for (let c of word) {
+                aL[c] = new Set();
+            }
+        }
+        for (let i = 0; i < words.length - 1; i++) {
+            let word1 = words[i],
+                word2 = words[i + 1];
+            if (
+                word1.length > word2.length &&
+                word1.slice(0, word2.length) === word2.slice(0, word2.length)
+            ) {
+                return "";
+            }
+            // start creating graph
+            for (let i = 0; i < word1.length; i++) {
+                if (word2[i] && word1[i] !== word2[i]) {
+                    aL[word1[i]].add(word2[i]);
+                    break;
+                }
+            }
+        }
+        let visited = {};
+        //visited false will track result is added or not and visited true will detect cycle and will return empty string
+        let result = [];
+        function dfs(n: string) {
+            if (visited.hasOwnProperty(n)) return visited[n];
+            visited[n] = true;
+            for (let ne of aL[n]) {
+                if (dfs(ne)) return false;
+            }
+            visited[n] = false;
+            result.push(n);
+        }
+        for (let key in aL) {
+            if (dfs(key)) return "";
+        }
+        return result.reverse().join("");
+    }
+    foreignDictionary(words: string[]): string {
+        let al = {};
+        for (let word of words) {
+            for (let char of word) {
+                al[char] = new Set();
+            }
+        }
+
+        for (let i = 0; i < words.length - 1; i++) {
+            let word1 = words[i],
+                word2 = words[i + 1];
+            let len = Math.min(word1.length, word2.length);
+            if (word1.length > word2.length && word1.slice(0, len) === word2.slice(0, len)) {
+                return '';
+            }
+
+            for (let i = 0; i < word1.length; i++) {
+                if (word1[i] !== word2[i]) {
+                    al[word1[i]].add(word2[i]);
+                    break;
+                }
+            }
+        }
+
+        let visited = {};
+        let result = [];
+        function traverse(n) {
+            if (visited.hasOwnProperty(n)) return visited[n];
+            visited[n] = true;
+            for (let ne of al[n]) {
+               if(traverse(ne)) return false;
+            }
+            visited[n] = false;
+            result.push(n);
+        }
+
+        for (let n in al) {
+            if (traverse(n)) return "";
+        }
+        return result.reverse().join("");
+    }
+}
