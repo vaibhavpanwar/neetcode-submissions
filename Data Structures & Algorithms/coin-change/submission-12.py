40b224f94c556a5857515f5c96bb122a21e9f3eb
Vaibhav Panwar <51419252+vaibhavpanwar@users.noreply.github.com>
2026-08-27 15:16:42 +0530

    Add: coin-change - submission-12

---
+++ Data Structures & Algorithms/coin-change/submission-12.py
@@ -0,0 +1,23 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [None] * (amount + 1)
+
+        def check(am):
+            if am == 0:
+                return 0
+            if am < 0:
+                return float("inf")
+            if dp[am] is not None:
+                return dp[am]
+
+            ways = float("inf")
+            for coin in coins:
+                remaining = am - coin
+                if remaining >= 0:
+                    ways = min(1 + check(remaining), ways)
+
+            dp[am] = ways
+            return ways
+
+        way = check(amount)
+        return -1 if way == float("inf") else way
